8b83cbdba358a224afc8dc2ecf3578a44db85f57
Michaela <michaela.winterbottom@wemakewebsites.info>
2019-10-04 14:24:21 +0100

    Sphere animation

--- dist/script.js
+++ dist/script.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 var scene = void 0,
     camera = void 0,
     renderer = void 0,
-    cube = void 0;
+    cube = void 0,
+    sphere = void 0;
 
 //how much to move cube in every call in loop
 var ADD = 0.1;
@@ -18,6 +19,15 @@ var createCube = function createCube() {
   var material = new THREE.MeshBasicMaterial({ color: 0x00a1cb });
   cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+};
+
+var createSphere = function createSphere() {
+  console.log('creating sphere');
+  //(radius/horizontal-vertical)
+  var geometry = new THREE.SphereGeometry(0.5, 30, 30, 0, Math.PI, 0, Math.PI / 2);
+  var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+  sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
 };
 
 //Set up the environment - 
@@ -31,11 +41,13 @@ var init = function init() {
 
   //create and locate the camera
   camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 5;
+  camera.position.z = 4;
 
   // show axis helper
-  // let axes = new THREE.AxesHelper(5);
-  // scene.add(axes);
+  var axes = new THREE.AxesHelper(5);
+  scene.add(axes);
+
+  createSphere();
 
   createCube();
 
@@ -49,12 +61,15 @@ var init = function init() {
 
 var mainLoop = function mainLoop() {
   //add ADD number on very loop.
-  // cube.position.x += ADD;
-  // cube.rotation.z -= ADD;
+  cube.position.x += ADD;
+  cube.rotation.z -= ADD;
 
   cube.rotation.y += ADD;
 
-  //reverse direction if reached 2 on either side
+  sphere.rotation.x += ADD;
+  // sphere.rotation.y += ADD;
+
+  // reverse direction if reached 2 on either side
   if (cube.position.x <= -2 || cube.position.x >= 2) {
     ADD *= -1;
   }
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   mainLoop();
 });
 
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, sphere;
 
 //how much to move cube in every call in loop
 let ADD = 0.1;
@@ -13,6 +13,15 @@ let createCube = function() {
   let material = new THREE.MeshBasicMaterial({color: 0x00a1cb});
   cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+}
+
+let createSphere = function() {
+  console.log('creating sphere');
+  //(radius/horizontal-vertical)
+  let geometry = new THREE.SphereGeometry(0.5, 30, 30, 0, Math.PI, 0, Math.PI/2);
+  let material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+  sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
 }
 
 //Set up the environment - 
@@ -26,13 +35,16 @@ let init = function() {
 
   //create and locate the camera
   camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 5;
+  camera.position.z = 4;
 
   // show axis helper
-  // let axes = new THREE.AxesHelper(5);
-  // scene.add(axes);
+  let axes = new THREE.AxesHelper(5);
+  scene.add(axes);
+
+  createSphere();
 
   createCube();
+
 
   //create the renderer
   renderer = new THREE.WebGLRenderer();
@@ -44,12 +56,15 @@ let init = function() {
 
 let mainLoop = function() {
   //add ADD number on very loop.
-  // cube.position.x += ADD;
-  // cube.rotation.z -= ADD;
+  cube.position.x += ADD;
+  cube.rotation.z -= ADD;
 
   cube.rotation.y += ADD;
 
-  //reverse direction if reached 2 on either side
+  sphere.rotation.x += ADD;
+  // sphere.rotation.y += ADD;
+
+  // reverse direction if reached 2 on either side
   if(cube.position.x <= -2 || cube.position.x >= 2) {
     ADD *= -1;
   }
